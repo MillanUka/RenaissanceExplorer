a3c299f7c3fd250828f3821fde865f4993bdf8a0
MillanUka <41311473+MillanUka@users.noreply.github.com>
2019-10-16 19:08:24 +1300

    Added some flag images

--- Scripts/map/florence.gd
+++ Scripts/map/florence.gd
@@ -12,13 +12,4 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == BUTTON_LEFT:
-        var pos = position + offset - ( (texture.get_size() / 2.0) if centered else Vector2() )
-        var size = texture.get_size()
-        print("Position: " + pos as String)
-        print("Size: " + size as String)
-        print("Event position: " + event.position as String)
-        if Rect2(pos, texture.get_size()).has_point(event.position):
-           print("It inside")
 	

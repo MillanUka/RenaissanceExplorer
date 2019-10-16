d8e0660f566620eb83e9a4b5d5e30b7c3cd50911
MillanUka <ukamillan@gmail.com>
2019-10-16 21:30:36 +1300

    Added a panel to display the information about a nation

--- Scripts/map/genoa.gd
+++ Scripts/map/genoa.gd
@@ -6,14 +6,8 @@ extends Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("test")
-	
-	
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-	
-
-
-func _on_Button_pressed():
-	print("Florence");

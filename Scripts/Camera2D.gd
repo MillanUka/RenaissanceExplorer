0519b1103441f96fbfde06841ec45e048de1fefe
MillanUka <41311473+MillanUka@users.noreply.github.com>
2019-11-02 09:50:24 +1300

    Changed the camera script

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -3,8 +3,11 @@ extends Camera2D
 func _input(event):
 	if event is InputEventMouseMotion:
 		var mouse_position = get_viewport().get_mouse_position() 
-		var x_offset = -10;
-		var y_offset = 10;
+		var screen_size = get_viewport().size;
+		var x_offset = mouse_position.x/screen_size.x
+		var y_offset = mouse_position.y/screen_size.y;
+		x_offset *= 0.5;
+		y_offset *= 0.5;
 		var vector = Vector2(mouse_position.x+x_offset,mouse_position.y+y_offset) 
 		self.position = vector 
 
